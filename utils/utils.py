eb2d6f317ed9b22b89307c5fc51e4bea9f25c7fa
dpbenalcazar <danielbenalcazar87@gmail.com>
2021-11-16 17:33:22 -0500

    Changed black mask limit to 4/255

--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 
 def get_mask(img):
-    mask_B = img[:,:,0] < 5
-    mask_G = img[:,:,1] < 5
-    mask_R = img[:,:,2] < 5
+    mask_B = img[:,:,0] < 4
+    mask_G = img[:,:,1] < 4
+    mask_R = img[:,:,2] < 4
     mask = mask_B & mask_G & mask_R
     mask = 255*mask.astype(np.uint8)
     mask = cv2.dilate(mask, None, iterations=2)
